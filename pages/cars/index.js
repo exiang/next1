24f0da3f6ab3419517de6913946501431eb42d3e
Yee Siang <exiang83@yahoo.com>
2022-04-10 20:50:05 +0800

    car list is dynamic acquired from api (json)

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,3 +1,38 @@
-export default function CarsList(){
-    return <h1>Cars List</h1>
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+import {useRouter} from 'next/router'
+import {useEffect, useState} from "react";
+import { Capitalize } from '../_helpers'
+
+export default function CarsList(props){
+    const [cars, setCars] = useState("");
+    useEffect(() => {
+        const url = "http://localhost:3000/cars.json";
+        const fetchCars = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                setCars(json);
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+        fetchCars();
+    }, []);
+    
+
+    return (
+    <div className={styles.container}>
+        <main className={styles.main}>
+        <h1 className={styles.title}>
+          Cars list
+        </h1>
+        {cars && cars.length?(
+            <ul>{cars.map(function(car, i){
+                return  <li><Link href={"/cars/"+car}><a>{Capitalize(car)}</a></Link></li> ;
+            })}</ul>
+        ):'cars loading...'}
+        </main>
+    </div>
+    )
 }
